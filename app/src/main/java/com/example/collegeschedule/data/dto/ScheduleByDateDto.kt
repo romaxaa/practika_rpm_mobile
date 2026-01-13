2f0eeaba1371e3ddaf5d74c19c823f74a0d9fb26
Roman <romkisner@gmail.com>
2026-01-13 16:01:23 +0700

    Добавили файл ScheduleByDateDto

---
+++ app/src/main/java/com/example/collegeschedule/data/dto/ScheduleByDateDto.kt
@@ -0,0 +1,8 @@
+package com.example.collegeschedule.data.dto
+
+data class ScheduleByDateDto(
+    val lessonDate: String, // ISO: 2026-01-12
+    val weekday: String,
+    val lessons: List<LessonDto>
+)
+
