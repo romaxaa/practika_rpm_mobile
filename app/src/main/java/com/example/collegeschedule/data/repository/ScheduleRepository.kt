282a723cec7d43d2db21ea91470d97e0aae7d8a1
Roman <romkisner@gmail.com>
2026-01-14 15:01:07 +0700

    Изменение кода для загрузки кода

--- app/src/main/java/com/example/collegeschedule/data/repository/ScheduleRepository.kt
+++ app/src/main/java/com/example/collegeschedule/data/repository/ScheduleRepository.kt
@@ -2,10 +2,11 @@ package com.example.collegeschedule.data.repository
 
 import com.example.collegeschedule.data.api.ScheduleApi
 import com.example.collegeschedule.data.dto.ScheduleByDateDto
+
 class ScheduleRepository(private val api: ScheduleApi) {
     suspend fun loadSchedule(group: String): List<ScheduleByDateDto> {
         return api.getSchedule(
-            groupName = group,
+            groupName = group,             // <- path param
             start = "2026-01-12",
             end = "2026-01-17"
         )
